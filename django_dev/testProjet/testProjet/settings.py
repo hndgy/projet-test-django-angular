acd8478e0c80ea9982fcfed22f1aa37d3af070d5
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2020-06-03 17:37:12 +0200

    front vraiment finis

--- django_dev/testProjet/testProjet/settings.py
+++ django_dev/testProjet/testProjet/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'z04(rl7+n4n+j%#+*2b=y#hhj1720**und^%gp4bs-)%)l)$45'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,7 +43,8 @@ INSTALLED_APPS = [
     'corsheaders'
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = [ 
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,8 +52,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware'
+   
+    'django.middleware.common.CommonMiddleware',
+ 
 ]
 
 ROOT_URLCONF = 'testProjet.urls'
